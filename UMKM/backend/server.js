4494e205a670a04819e493bd302d3f3b1e669225
ucii26 <uciinentoo@outlook.com>
2025-07-11 01:36:25 +0700

    Update server.js

--- UMKM/backend/server.js
+++ UMKM/backend/server.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+const umkmRoutes = require("./routes/umkmRoutes");
 
+app.use(cors());
+app.use(express.json());
+app.use("/api/umkm", umkmRoutes);
+
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
